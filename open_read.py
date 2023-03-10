1941c69b41803aac6d2b3d12c2846ea4d18ade1d
xibet2073 <fosol2073@gmail.com>
2023-03-10 19:07:04 +0100

    add csv read/write scripts

---
+++ open_read.py
@@ -0,0 +1,15 @@
+import csv
+
+with open('csv/employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            if len(row) >= 3:
+                print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
